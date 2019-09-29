214d4d855c905c77d1c5215c75dee80a4e34788f
Nikolay Dyahovets <kdeagovec@gmail.com>
2019-09-29 22:24:14 +0300

    Have implemented add, remove, update user.

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,54 +1,66 @@
-import {call, put, takeEvery, all} from "redux-saga/effects";
+import {call, put, takeEvery, all, select} from "redux-saga/effects";
+import { getUsers } from "../selectors";
 
-export const FETCHED_ALL_USERS = 'FETCHED_ALL_USERS';
-export const REQUESTED_ALL_USERS = 'REQUESTED_ALL_USERS';
-export const REQUESTED_ALL_USERS_SUCCEEDED = 'REQUESTED_ALL_USERS_SUCCEEDED';
-export const REQUESTED_ALL_USERS_FAILED = 'REQUESTED_ALL_USERS_FAILED';
+export const GET_ALL_USERS = 'GET_ALL_USERS';
+export const GET_ALL_USERS_START = 'GET_ALL_USERS_START';
+export const GET_ALL_USERS_SUCCESS = 'GET_ALL_USERS_SUCCESS';
+export const GET_ALL_USERS_FAIL = 'GET_ALL_USERS_FAIL';
 
 export const POST_NEW_USER_START = 'POST_NEW_USER_START';
 export const POST_NEW_USER_SUCCESS = 'POST_NEW_USER_SUCCESS';
 export const POST_NEW_USER_FAIL = 'POST_NEW_USER_FAIL';
 export const POST_NEW_USER = 'POST_NEW_USER';
 
-const requestAllUsers = () => {
+export const DELETE_USER = 'DELETE_USER';
+export const DELETE_USER_START = 'DELETE_USER_START';
+export const DELETE_USER_SUCCESS = 'DELETE_USER_SUCCESS';
+export const DELETE_USER_FAIL = 'DELETE_USER_FAIL';
+
+export const UPDATE_USER = 'UPDATE_USER';
+export const UPDATE_USER_START = 'UPDATE_USER_START';
+export const UPDATE_USER_SUCCESS = 'UPDATE_USER_SUCCESS';
+export const UPDATE_USER_FAIL = 'UPDATE_USER_FAIL';
+
+const getAllUsersStart = () => {
     return {
-        type: REQUESTED_ALL_USERS,
+        type: GET_ALL_USERS_START,
     };
 };
 
-const requestAllUsersSuccess = (data) => {
+const getAllUsersSuccess = (data) => {
     return {
-        type: REQUESTED_ALL_USERS_SUCCEEDED,
+        type: GET_ALL_USERS_SUCCESS,
         users: data,
     };
 };
 
-const requestAllUsersError = () => {
+const getAllUsersFail = (errorMessage) => {
     return {
-        type: REQUESTED_ALL_USERS_FAILED,
+        type: GET_ALL_USERS_FAIL,
+        errorMessage
     };
 };
 
-export const fetchAllUsers = () => {
+export const getAllUsers = () => {
     return {
-        type: FETCHED_ALL_USERS,
+        type: GET_ALL_USERS,
     };
 };
 
 function* watchFetchAllUsers() {
-    yield takeEvery(FETCHED_ALL_USERS, fetchAllUsersAsync);
+    yield takeEvery(GET_ALL_USERS, fetchAllUsersAsync);
 }
 
 function* fetchAllUsersAsync() {
     try {
-        yield put(requestAllUsers());
+        yield put(getAllUsersStart());
         const usersData = yield call(() => {
             return fetch('https://localhost:5001/api/users')
                 .then(res => res.json());
         });
-        yield put(requestAllUsersSuccess(usersData));
+        yield put(getAllUsersSuccess(usersData));
     } catch (error) {
-        yield put(requestAllUsersError());
+        yield put(getAllUsersFail(error));
     }
 }
 
@@ -67,9 +79,10 @@ const postNewUserSuccess = (newUser) => {
     };
 };
 
-const postNewUserFail = () => {
+const postNewUserFail = (errorMessage) => {
     return {
         type: POST_NEW_USER_FAIL,
+        errorMessage,
     };
 };
 
@@ -87,6 +100,8 @@ function* watchPostNewUser() {
 function* postNewUserAsync(action) {
     try {
         yield put(postNewUserStart());
+
+        // I get new user and add him to the users in the store.
         const newUser = yield call(() => {
             return fetch('https://localhost:5001/api/users', {
                 method: "POST",
@@ -98,7 +113,130 @@ function* postNewUserAsync(action) {
         });
         yield put(postNewUserSuccess(newUser));
     } catch (error) {
-        yield put(postNewUserFail());
+        yield put(postNewUserFail(error));
+    }
+}
+
+//====================================================================
+
+const deleteUserStart = () => {
+    return {
+        type: DELETE_USER_START,
+    }
+};
+
+const deleteUserSuccess = (usersWithoutDeletedUser) => {
+    return {
+        type: DELETE_USER_SUCCESS,
+        usersWithoutDeletedUser,
+    }
+};
+
+const deleteUserFail = (errorMessage) => {
+    return {
+        type: DELETE_USER_FAIL,
+        errorMessage,
+    }
+};
+
+export const deleteUser = (user) => {
+    return {
+        type: DELETE_USER,
+        user,
+    }
+};
+
+function* watchDeleteUser() {
+    yield takeEvery(DELETE_USER, deleteUserAsync);
+}
+
+function* deleteUserAsync(action) {
+    try {
+        // users from state.
+        const users = yield select(getUsers);
+        // user from action.
+        const userToDelete = yield action.user;
+        // user index in state is in property tableData of the userToDelete.
+        const userIndexInState = yield userToDelete.tableData.id;
+        // delete user from state.
+        yield users.splice(userIndexInState, 1);
+
+        //uses id for deleting in API.
+        const userId = yield action.user.id;
+
+        yield put(deleteUserStart());
+
+        // I get deleted user from API and delete him from the users in the store.
+        yield call(() => {
+            return fetch(`https://localhost:5001/api/users/${userId}`, {
+                method: "DELETE",
+            }).then(res => res.json());
+        });
+        yield put(deleteUserSuccess(users));
+    } catch (error) {
+        yield put(deleteUserFail(error));
+    }
+}
+
+//====================================================================
+
+export const updateUser = (updatedUser) => {
+    return {
+        type: UPDATE_USER,
+        updatedUser,
+    };
+};
+
+const updateUserStart = () => {
+    return {
+        type: UPDATE_USER_START,
+    };
+};
+
+const updateUserSuccess = (usersWithUpdatedUser) => {
+    return {
+        type: UPDATE_USER_SUCCESS,
+        usersWithUpdatedUser,
+    };
+};
+
+const updateUserFail = (errorMessage) => {
+    return {
+        type: UPDATE_USER_FAIL,
+        errorMessage,
+    };
+};
+
+function* watchUpdateUser() {
+    yield takeEvery(UPDATE_USER, updateUserAsync);
+}
+
+function* updateUserAsync(action) {
+    try {
+        const usersFromState = yield select(getUsers);
+        const userIdInTableData = yield action.updatedUser.tableData.id;
+        const {id, name, age} = yield action.updatedUser;
+        const userToUpdate = yield {
+            id,
+            name,
+            age,
+        };
+        const userToUpdateInJSON = yield JSON.stringify(userToUpdate);
+        yield usersFromState[userIdInTableData] = userToUpdate;
+        const allUsersWithUpdatedUser = yield usersFromState;
+
+        yield put(updateUserStart());
+        yield call(() => fetch('https://localhost:5001/api/users', {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: userToUpdateInJSON,
+            }).then((response) => response.json())
+        );
+        yield put(updateUserSuccess(allUsersWithUpdatedUser));
+    } catch (error) {
+        yield put(updateUserFail(error));
     }
 }
 
@@ -107,9 +245,11 @@ function* postNewUserAsync(action) {
 export function* rootSaga() {
     yield all([
         watchFetchAllUsers(),
-        watchPostNewUser()
+        watchPostNewUser(),
+        watchDeleteUser(),
+        watchUpdateUser(),
     ]);
     // It is the same as above.
-    // yield takeEvery(FETCHED_ALL_USERS, fetchAllUsersAsync);
+    // yield takeEvery(GET_ALL_USERS, fetchAllUsersAsync);
     // yield takeEvery(POST_NEW_USER, postNewUserAsync);
 }
